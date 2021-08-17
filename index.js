a494b8301397ea8e7c1562e4fb4b62d0344909e3
tpanzini <tpanzini4@gmail.com>
2021-08-17 18:09:47 -0400

    add failure and success pages for sign up

--- index.js
+++ index.js
@@ -1,132 +1,76 @@
-// const carouselSlide = document.querySelector(".slider-container");
-// const carouselImages = document.querySelectorAll(".slider-container img");
-// const img = document.querySelector(".slider-container img");
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+const token = config.MY_API_TOKEN;
 
-//Button
-// const btnLeft = document.querySelector(".btn-left");
-// const btnRight = document.querySelector(".btn-right");
+const app = express();
 
-//thumbnails
-// const tokyoThumbnail = document.querySelector(".tokyo-thumbnail");
-// const osakaThumbnail = document.querySelector(".osaka-thumbnail");
-// const naraThumbnail = document.querySelector(".nara-thumbnail");
-// const kyotoThumbnail = document.querySelector(".kyoto-thumbnail");
-//counter
-// let counter = 0;
-// const size = carouselImages[0].clientWidth;
-// const sizeRem = size / 16;
-// const getWidthElement = window.getComputedStyle("img");
-// console.log(getWidthElement);
-// const size = getWidthElement.getPropertyValue("width");
-//720px
-
-// function nextSlide() {
-//   if (counter >= carouselImages.length - 1) {
-//     carouselSlide.style.transition = "transform 0.4s ease-in-out";
-//     carouselSlide.style.transform =
-//       "translateX(" + -sizeRem * carouselImages.length + "rem)";
-//     counter = -1;
-//   }
-//   counter++;
-//   carouselSlide.style.transition = "transform 0.4s ease-in-out";
-//   carouselSlide.style.transform = "translateX(" + -sizeRem * counter + "rem)";
-// }
-
-// function prevSlide() {
-//   if (counter <= 0) {
-//     carouselSlide.style.transition = "transform 0.4s ease-in-out";
-//     carouselSlide.style.transform =
-//       "translateX(" + -sizeRem * (carouselImages.length - 1) + "rem)";
-//     counter = carouselImages.length;
-//   }
-//   counter--;
-//   carouselSlide.style.transition = "transform 0.4s ease-in-out";
-//   carouselSlide.style.transform = "translateX(" + -sizeRem * counter + "rem)";
-// }
-
-//change slider function
-// function changeSlider(e) {
-//   const oldThumbnail = e.target;
-//   const newThumbnail = document.querySelector(".slider-container img").src;
-//   const titleText = oldThumbnail.getAttribute("alt");
-//   carouselSlide.innerHTML = "";
-//   const firstImage = oldThumbnail.src;
-//   oldThumbnail.setAttribute("src", newThumbnail);
-//   console.log(e);
-
-//   const slide = document.createElement("div");
-// let name = n;
-// slide.innerHTML = ` <div class="image slide-1">
-//     <h2 class=" title-text">${titleText}</h2>
-//     <p class="slider-text">From full-time guided packages for completly stress free trips, to fully tailored itenaries for those who choose to venture alone.</p>
-//     <img class="image" src=${firstImage}>
-//   </div>
-//    <div class="slide-2">
-//     <h2 class=" title-text">${titleText}</h2>
-//     <p class="slider-text">>From full-time guided packages for completly stress free trips, to fully tailored itenaries for those who choose to venture alone.</p>
-//     <img class="image" src=/${e.target.alt}-2.jpg>
-//   </div>
-//    <div class="slide-3">
-//     <h2 class="title-text">${titleText}</h2>
-//     <p class="slider-text">>From full-time guided packages for completly stress free trips, to fully tailored itenaries for those who choose to venture alone.</p>
-//     <img class="image" src=/${e.target.alt}-3.jpg>
-//   </div>
-//    <div class="slide-4">
-//     <h2 class="title-text">${titleText}</h2>
-//     <p class="slider-text">From full-time guided packages for completly stress free trips, to fully tailored itenaries for those who choose to venture alone.</p>
-//     <img class="image" src=/${e.target.alt}-4.jpg>
-//   </div>`;
-//save each image as the Alt text - number (i.e. "Osaka-2")
-// carouselSlide.appendChild(slide);
-// }
-
-//event listeners
-//slider buttons
-// btnRight.addEventListener("click", nextSlide);
-// btnLeft.addEventListener("click", prevSlide);
-
-//thumbnail listenrs
-// tokyoThumbnail.addEventListener('click', changeSlider);
-// osakaThumbnail.addEventListener("click", changeSlider);
-// naraThumbnail.addEventListener("click", changeSlider);
-// kyotoThumbnail.addEventListener("click", changeSlider);
-
-//Slide in Content
-const scrollElements = document.querySelectorAll(".js-scroll");
-
-const elementInView = (el, dividend = 1) => {
-  const elementTop = el.getBoundingClientRect().top;
-  return (
-    elementTop <=
-    (window.innerHeight || document.documentElement.clientHeight) / dividend
-  );
-};
-
-const elementOutofView = (el) => {
-  const elementTop = el.getBoundingClientRect().top;
-
-  return (
-    elementTop > (window.innerHeight || document.documentElement.clientHeight)
-  );
-};
-
-const displayScrollElement = (element) => {
-  element.classList.add("scrolled");
-};
-
-const hideScrollElement = (element) => {
-  element.classList.remove("scrolled");
-};
-
-const handleScrollAnimation = () => {
-  scrollElements.forEach((el) => {
-    if (elementInView(el, 1.25)) {
-      displayScrollElement(el);
-    } else if (elementOutofView(el)) {
-      hideScrollElement(el);
-    }
-  });
-};
-window.addEventListener("scroll", () => {
-  handleScrollAnimation();
+app.listen(5500, function () {
+  console.log("server running");
 });
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function (req, res) {
+  const firstName = req.body.Fname;
+  const lastName = req.body.Lname;
+  const email = req.body.email;
+  console.log(email);
+
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName,
+        },
+      },
+    ],
+  };
+
+  const jsonData = JSON.stringify(data);
+
+  const url = "https://us18.api.mailchimp.com/3.0/lists/095363ddda";
+  const options = {
+    method: "POST",
+    auth: `travis:${token}`,
+  };
+
+  const request = https.request(url, options, function (response) {
+    response.on("data", function (data) {
+      if (response.statusCode === 200) {
+        res.sendFile(__dirname + "/success.html");
+      } else {
+        res.sendFile(__dirname + "/failure.html");
+      }
+
+      response.on("data", function (data) {
+        console.log(JSON.parse(data));
+      });
+    });
+  });
+
+  request.write(jsonData);
+  request.end();
+});
+
+app.post("/failure", function (req, res) {
+  res.redirect("/");
+});
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log("server is running");
+});
+
+// API Key
+// d5485a69957c1335fe4456bd07a7236c-us18
+
+//list id 095363ddda
